de1a3d2f1cd896734858cbb6bb4eff5fc892f71b
tadashi-aikawa <syou.maman@gmail.com>
2017-07-06 09:07:01 +0900

    :new: Add basic features

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,18 +1,13 @@
-import * as React from "react";
-import * as _ from "lodash";
-import {
-    Card,
-    Icon,
-    Image,
-    Feed,
-    Statistic,
-    Segment,
-    Label
-} from 'semantic-ui-react'
+import * as React from 'react';
+import * as _ from 'lodash';
 import {Dictionary} from 'lodash';
-import {fetchTasks} from "../client/TodoistClient";
+import {Card, Feed, Label, Segment, Statistic} from 'semantic-ui-react';
 import Emojify from 'react-emojione';
+import * as moment from 'moment';
 
+
+const MINUTES = 240;
+const DATE_FORMAT = "YYYY-MM-DD (ddd)";
 
 export interface Task {
     name: string;
@@ -34,9 +29,9 @@ export const TaskFeed = (props: TaskFeedProps) =>
             <Feed.Date content={props.project} />
             <Feed.Summary>
                 <Emojify style={{height: 20, width: 20}}>{props.name}</Emojify>
-                <Label color='teal' circular>{props.elapsedMinutes}</Label>
             </Feed.Summary>
         </Feed.Content>
+        <Label color='teal' circular style={{width:25, height:20, textAlign: 'center'}}>{props.elapsedMinutes}</Label>
     </Feed.Event>;
 
 
@@ -45,22 +40,44 @@ export interface DailyCardProps {
     tasks: Task[];
 }
 
-export const DailyCard = (props: DailyCardProps) =>
-    <Card style={{width: 400}}>
+export const DailyCard = (props: DailyCardProps) => {
+    const totalElapsedMinutes = _.sumBy(props.tasks, t => t.elapsedMinutes);
+    const colorMap = (time: number) => {
+        if (time >= 0) return "green";
+        if (time < 0) return "orange";
+        if (time < 1) return "violet";
+        if (time < 2) return "red";
+    };
+    const freeMinutes = MINUTES - totalElapsedMinutes;
+
+    return <Card>
         <Segment inverted>
-            <Statistic size='mini' color="olive" inverted="true">
-                <Statistic.Value>{new Date(props.date).toLocaleDateString()}</Statistic.Value>
+            <Statistic size='mini' color="olive">
+                <Statistic.Value>{moment(props.date).format(DATE_FORMAT)}</Statistic.Value>
             </Statistic>
-            <Label color='teal' circular>{Math.ceil(_.sumBy(props.tasks, t => t.elapsedMinutes) / 60 * 100) / 100} h</Label>
+            <Label.Group>
+                <Label color={colorMap(freeMinutes)}>
+                    Total
+                    <Label.Detail>{totalElapsedMinutes}</Label.Detail>
+                </Label>
+                <Label color={colorMap(freeMinutes)}>
+                    Reminding
+                    <Label.Detail>{freeMinutes}</Label.Detail>
+                </Label>
+            </Label.Group>
         </Segment>
         <Card.Content>
             <Feed>
-                {_.map(props.tasks, t =>
-                    <TaskFeed name={t.name} project={t.projectName} elapsedMinutes={t.elapsedMinutes} />
+                {_.map(props.tasks, (t, i) =>
+                    <TaskFeed key={i}
+                              name={t.name}
+                              project={t.projectName}
+                              elapsedMinutes={t.elapsedMinutes} />
                 )}
             </Feed>
         </Card.Content>
     </Card>;
+};
 
 export interface TopProps {
     tasks: Task[];
@@ -68,13 +85,23 @@ export interface TopProps {
 
 export const Top = (props: TopProps) => {
     const tasksByDueDate: Dictionary<Task[]> = _(props.tasks)
-        .groupBy(t => t.dueDate)
-        .toPairs().sortBy(x => Date.parse(x[0])).fromPairs()
+        .groupBy(t => moment(t.dueDate).format(DATE_FORMAT))
+        .toPairs().sortBy(x => x[0]).fromPairs()
+        .value();
+    const dates = _(_.range(0, 30))
+        .map(i => moment().startOf('week').add(i, 'day'))
+        .filter(d => d.day() > 0 && d.day() < 6)
         .value();
 
+
     return <div style={{padding: 10}}>
-        <Card.Group>
-            {_.map(tasksByDueDate, (tasks, dueDate) => <DailyCard date={dueDate} tasks={tasks}/>)}
+        <Card.Group itemsPerRow={5}>
+            {_.map(dates, d => {
+                const dateStr = moment(d).format(DATE_FORMAT);
+                return <DailyCard key={dateStr}
+                                  date={dateStr}
+                                  tasks={tasksByDueDate[dateStr]}/>
+            })}
         </Card.Group>
     </div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import '../node_modules/semantic-ui-css/semantic.min.css';
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as _ from "lodash";
-
-import {Task, Top} from "./components/Top";
-import {fetchTasks, fetchProjects} from './client/TodoistClient';
-import TodoistTask from './models/todoist/TodoistTask';
-import Project from './models/todoist/Project';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as _ from 'lodash';
 import {Dictionary} from 'lodash';
 
-const HOGEHOGE = 'TODO'
+import {Task, Top} from './components/Top';
+import {fetchProjects, fetchTasks} from './client/TodoistClient';
+import Project from './models/todoist/Project';
 
+const HOGEHOGE = 'TODO';
 const LABELS = {
     760006: 5,
     760002: 15,
@@ -23,14 +21,16 @@ Promise.all([fetchProjects(HOGEHOGE), fetchTasks(HOGEHOGE)])
     .then(([projects, todoistTasks]) => {
         const projectsById: Dictionary<Project> = _.keyBy(projects, p => p.id);
 
-        const tasks: Task[] = todoistTasks.filter(x => !x.checked)
-            .filter(x => x.labels.some(l => l in LABELS))
+        const tasks: Task[] = _(todoistTasks)
+            .filter(x => !x.checked && x.due_date_utc && x.labels.some(l => l in LABELS))
+            .orderBy(x => x.project_id)
             .map(x => ({
                 name: x.content,
                 projectName: projectsById[String(x.project_id)].name,
                 elapsedMinutes: _.find(LABELS, (v, k) => _.includes(x.labels, Number(k))),
                 dueDate: x.due_date_utc
-            }));
+            }))
+            .value();
 
         ReactDOM.render(
             <Top tasks={tasks}/>,
