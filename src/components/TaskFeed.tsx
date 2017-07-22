d35e2f4f9d383142d8280a3268ac9eb908907490
tadashi-aikawa <syou.maman@gmail.com>
2017-07-22 20:58:05 +0900

    :new: Add support for emoji in project name

--- src/components/TaskFeed.tsx
+++ src/components/TaskFeed.tsx
@@ -13,9 +13,9 @@ export const TaskFeed = (props: TaskFeedProps) =>
     <Feed.Event>
         <Feed.Label image={props.iconUrl}/>
         <Feed.Content>
-            <Feed.Date content={props.project}/>
+            <Feed.Date content={<Emojify style={{height: 20, width: 20}}>{props.project}</Emojify>}/>
             <Feed.Summary>
-                <Emojify style={{height: 20, width: 20}}>{props.name}</Emojify>
+                <Emojify style={{height: 20, width: 20, marginLeft: 10}}>{props.name}</Emojify>
             </Feed.Summary>
         </Feed.Content>
         <Label color='teal' circular style={{width: 25, height: 20, textAlign: 'center'}}>{props.estimatedMinutes}</Label>
