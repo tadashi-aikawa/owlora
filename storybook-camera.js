6b9911763e153a8a2f743d78fd5e1820da600931
tadashi-aikawa <syou.maman@gmail.com>
2018-04-08 22:57:28 +0900

    :hammer: Fix Dockerfile and puppeteer args

--- storybook-camera.js
+++ storybook-camera.js
@@ -18,7 +18,7 @@ server = app.listen(config.port);
 
 (async () => {
   console.log('Open browser')
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
   for (const kind of Object.keys(config.storiesByKind)) {
