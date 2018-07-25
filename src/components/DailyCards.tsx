cf02b98614b0df7ae38fff1ee3d9e3f1f215821c
tadashi-aikawa <syou.maman@gmail.com>
2018-07-25 23:42:29 +0900

    :hammer: Add tests

--- src/components/DailyCards.tsx
+++ src/components/DailyCards.tsx
@@ -1,92 +1,58 @@
-import * as React from 'react';
-import * as _ from 'lodash';
-import {Dictionary} from 'lodash';
-import {Card, SemanticWIDTHS} from 'semantic-ui-react';
-import {Moment} from 'moment';
-import {SIMPLE_FORMAT} from '../storage/settings';
-import Task, {TaskUpdateParameter} from '../models/Task';
-import DailyCard from './DailyCard';
-import TaskSortField from '../constants/TaskSortField';
-import Order from '../constants/Order';
-import * as DateUtil from '../utils/DateUtil';
-import Filter from '../models/Filter';
-import {plusDays, toStartDayOfWeek} from '../utils/DateUtil';
-
-
-const inTheDay = (task: Task, date: Moment): boolean => {
-    if (!task.dueDate) {
-        return false;
-    }
-
-    if (task.repetition && task.repetition.datesExcepted.some(d => d.isSame(date, "date"))) {
-        return false;
-    }
-
-    if (date.format(SIMPLE_FORMAT) === task.dueDate.format(SIMPLE_FORMAT)) {
-        return true;
-    }
-
-    if (!task.repetition || !date.isSameOrAfter(task.dueDate)) {
-        return false;
-    }
-
-    if (!_.includes<number>(task.repetition.dayOfWeek, date.day())) {
-        return false;
-    }
-
-    const behindWeek: number = toStartDayOfWeek(date).diff(toStartDayOfWeek(task.dueDate), 'week');
-    if (task.repetition.week === "every other" && behindWeek % 2 !== 0) {
-        return false;
-    }
-
-    return true;
-};
+import * as React from "react"
+import { Dictionary } from "lodash"
+import { Card, SemanticWIDTHS } from "semantic-ui-react"
+import { Moment } from "moment"
+import Task, { TaskUpdateParameter } from "../models/Task"
+import DailyCard from "./DailyCard"
+import TaskSortField from "../constants/TaskSortField"
+import Order from "../constants/Order"
+import Filter from "../models/Filter"
+import * as DateUtil from "../utils/DateUtil"
+import { plusDays, toStartDayOfWeek } from "../utils/DateUtil"
 
 function* momentIterator(begin: Moment, filter: (m: Moment) => boolean, max: number): IterableIterator<Moment> {
-    let i = 0;
-    let count = 0;
+    let i = 0
+    let count = 0
 
     while (count < max) {
-        const m = plusDays(begin, i);
+        const m = plusDays(begin, i)
         if (filter(m)) {
-            yield m;
-            count++;
+            yield m
+            count++
         }
-        i++;
+        i++
     }
 }
 
-
 export interface DailyCardsProps {
-    baseDate: Moment;
-    tasks: Task[];
-    taskSortField: TaskSortField;
-    taskOrder: Order;
-    timeLamps: boolean;
-    milestone: boolean;
-    seal: boolean;
-    warning: boolean;
-    isTasksExpanded: boolean;
-    minutesToUsePerDay: number;
-    minutesToUsePerSpecificDays: Dictionary<number>;
-    numberOfCards: number;
-    numberOfCardsPerRow: SemanticWIDTHS;
-    onlyWeekday: boolean;
-    filter?: Filter;
+    baseDate: Moment
+    tasks: Task[]
+    taskSortField: TaskSortField
+    taskOrder: Order
+    timeLamps: boolean
+    milestone: boolean
+    seal: boolean
+    warning: boolean
+    isTasksExpanded: boolean
+    minutesToUsePerDay: number
+    minutesToUsePerSpecificDays: Dictionary<number>
+    numberOfCards: number
+    numberOfCardsPerRow: SemanticWIDTHS
+    onlyWeekday: boolean
+    filter?: Filter
 
-    onUpdateTask: (parameter: TaskUpdateParameter) => void;
-    onRemoveTask: (id: number) => void;
+    onUpdateTask: (parameter: TaskUpdateParameter) => void
+    onRemoveTask: (id: number) => void
 }
 
 export const DailyCards = (props: DailyCardsProps) => {
-
     const dates: Moment[] = Array.from(
         momentIterator(
             toStartDayOfWeek(props.baseDate),
             props.onlyWeekday ? DateUtil.isWeekDay : () => true,
             props.numberOfCards
         )
-    );
+    )
 
     return (
         <Card.Group itemsPerRow={props.numberOfCardsPerRow}>
@@ -94,7 +60,7 @@ export const DailyCards = (props: DailyCardsProps) => {
                 <DailyCard
                     key={date.toString()}
                     date={date}
-                    tasks={props.tasks.filter(t => inTheDay(t, date))}
+                    tasks={props.tasks.filter(t => DateUtil.inTheDay(t, date))}
                     taskSortField={props.taskSortField}
                     taskOrder={props.taskOrder}
                     timeLamps={props.timeLamps}
@@ -105,11 +71,11 @@ export const DailyCards = (props: DailyCardsProps) => {
                     minutesToUsePerDay={props.minutesToUsePerDay}
                     minutesToUsePerSpecificDays={props.minutesToUsePerSpecificDays}
                     filter={props.filter}
-                    past={date.isBefore(props.baseDate, 'day')}
+                    past={date.isBefore(props.baseDate, "day")}
                     onUpdateTask={props.onUpdateTask}
                     onRemoveTask={props.onRemoveTask}
                 />
             ))}
         </Card.Group>
-    );
-};
+    )
+}
