4c11cf6449defb2e8cbfc6c8a662068cb22128b9
tadashi-aikawa <syou.maman@gmail.com>
2018-07-25 17:53:07 +0900

    :hammer: Refactoring tests

--- src/utils/DateUtil.spec.ts
+++ src/utils/DateUtil.spec.ts
@@ -5,7 +5,7 @@ import * as moment from "moment"
 const FORMAT = "YYYY/M/D"
 const d = (v: string): Moment => moment(v, FORMAT)
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${false}
 ${"2018/07/23"} | ${"Monday"}    | ${true}
@@ -14,9 +14,11 @@ ${"2018/07/25"} | ${"Wednesday"} | ${true}
 ${"2018/07/26"} | ${"Thursday"}  | ${true}
 ${"2018/07/27"} | ${"Friday"}    | ${true}
 ${"2018/07/28"} | ${"Saturday"}  | ${false}
-`("[isWeekDay] $expected if $dayOfWeek", ({ date, expected }) => expect(isWeekDay(d(date))).toEqual(expected))
+`("isWeekDay returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}`, () => expect(isWeekDay(d(date))).toEqual(expected))
+})
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${false}
 ${"2018/07/23"} | ${"Monday"}    | ${true}
@@ -25,9 +27,11 @@ ${"2018/07/25"} | ${"Wednesday"} | ${false}
 ${"2018/07/26"} | ${"Thursday"}  | ${false}
 ${"2018/07/27"} | ${"Friday"}    | ${false}
 ${"2018/07/28"} | ${"Saturday"}  | ${false}
-`("[isMonDay] $expected if $dayOfWeek", ({ date, expected }) => expect(isMonDay(d(date))).toEqual(expected))
+`("isMonday returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}`, () => expect(isMonDay(d(date))).toEqual(expected))
+})
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${false}
 ${"2018/07/23"} | ${"Monday"}    | ${false}
@@ -36,9 +40,11 @@ ${"2018/07/25"} | ${"Wednesday"} | ${false}
 ${"2018/07/26"} | ${"Thursday"}  | ${false}
 ${"2018/07/27"} | ${"Friday"}    | ${false}
 ${"2018/07/28"} | ${"Saturday"}  | ${false}
-`("[isTuesDay] $expected if $dayOfWeek", ({ date, expected }) => expect(isTuesDay(d(date))).toEqual(expected))
+`("isTuesDay returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}`, () => expect(isTuesDay(d(date))).toEqual(expected))
+})
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${false}
 ${"2018/07/23"} | ${"Monday"}    | ${false}
@@ -47,9 +53,11 @@ ${"2018/07/25"} | ${"Wednesday"} | ${true}
 ${"2018/07/26"} | ${"Thursday"}  | ${false}
 ${"2018/07/27"} | ${"Friday"}    | ${false}
 ${"2018/07/28"} | ${"Saturday"}  | ${false}
-`("[isWednesDay] $expected if $dayOfWeek", ({ date, expected }) => expect(isWednesDay(d(date))).toEqual(expected))
+`("isWednesDay returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}`, () => expect(isWednesDay(d(date))).toEqual(expected))
+})
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${false}
 ${"2018/07/23"} | ${"Monday"}    | ${false}
@@ -58,9 +66,11 @@ ${"2018/07/25"} | ${"Wednesday"} | ${false}
 ${"2018/07/26"} | ${"Thursday"}  | ${true}
 ${"2018/07/27"} | ${"Friday"}    | ${false}
 ${"2018/07/28"} | ${"Saturday"}  | ${false}
-`("[isThursDay] $expected if $dayOfWeek", ({ date, expected }) => expect(isThursDay(d(date))).toEqual(expected))
+`("isThursDay returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}`, () => expect(isThursDay(d(date))).toEqual(expected))
+})
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${false}
 ${"2018/07/23"} | ${"Monday"}    | ${false}
@@ -69,9 +79,11 @@ ${"2018/07/25"} | ${"Wednesday"} | ${false}
 ${"2018/07/26"} | ${"Thursday"}  | ${false}
 ${"2018/07/27"} | ${"Friday"}    | ${true}
 ${"2018/07/28"} | ${"Saturday"}  | ${false}
-`("[isFriDay] $expected if $dayOfWeek", ({ date, expected }) => expect(isFriDay(d(date))).toEqual(expected))
+`("isFriDay returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}`, () => expect(isFriDay(d(date))).toEqual(expected))
+})
 
-test.each`
+describe.each`
 date            | dayOfWeek      | expected
 ${"2018/07/22"} | ${"Sunday"}    | ${"2018/7/22"}
 ${"2018/07/23"} | ${"Monday"}    | ${"2018/7/22"}
@@ -80,6 +92,6 @@ ${"2018/07/25"} | ${"Wednesday"} | ${"2018/7/22"}
 ${"2018/07/26"} | ${"Thursday"}  | ${"2018/7/22"}
 ${"2018/07/27"} | ${"Friday"}    | ${"2018/7/22"}
 ${"2018/07/28"} | ${"Saturday"}  | ${"2018/7/22"}
-`("[toStartDayOfWeek] Sunday if $dayOfWeek", ({ date, expected }) => {
-    expect(toStartDayOfWeek(d(date)).format(FORMAT)).toEqual(expected)
+`("toStartDayOfWeek returns", ({ date, dayOfWeek, expected }) => {
+    test(`${expected} if ${dayOfWeek}(${date})`, () => expect(toStartDayOfWeek(d(date)).format(FORMAT)).toEqual(expected))
 })
