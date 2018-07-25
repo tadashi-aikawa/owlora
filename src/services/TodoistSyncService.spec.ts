0d3e68c260647b1e781c3bdd78e13e798b4d6d98
tadashi-aikawa <syou.maman@gmail.com>
2018-07-25 20:08:46 +0900

    :hammer: Add tests

--- src/services/TodoistSyncService.spec.ts
+++ src/services/TodoistSyncService.spec.ts
@@ -1,10 +1,10 @@
-import { Moment } from "moment"
-import * as moment from "moment"
-import { toRepetition } from "./TodoistSyncService"
-import { Repetition } from "../models/Repetition"
+import {toRepetition} from "./TodoistSyncService"
+import {Repetition} from "../models/Repetition"
 
 const FORMAT = "YYYY/M/D"
-const d = (v: string): Moment => moment(v, FORMAT)
+// now is 2000/07/01 !!!
+require('jest-mock-now')(new Date('2000-07-01'));
+
 
 describe.each`
 dateString                | content        | exDay        | exDayOfWeek               | exWeek           | exMonth      | exDatesExcepted
@@ -24,5 +24,25 @@ ${"every other mon,wed"}  | ${"task"}      | ${"every"}   | ${[1, 3]}           
         test(`week is ${exWeek}`, () => expect(r.week).toEqual(exWeek))
         test(`month is ${exMonth}`, () => expect(r.month).toEqual(exMonth))
         test(`datesExpected is ${exDatesExcepted}`, () => expect(r.datesExcepted).toEqual(exDatesExcepted))
+    }
+)
+
+describe.each`
+dateString        | content                    | exDay      | exDayOfWeek | exWeek     | exMonth    | exDatesExcepted
+${"every monday"} | ${"task x7/20"}            | ${"every"} | ${[1]}      | ${"every"} | ${"every"} | ${["2000/7/20"]}
+${"every monday"} | ${"task x7/22"}            | ${"every"} | ${[1]}      | ${"every"} | ${"every"} | ${["2000/7/22"]}
+${"every monday"} | ${"task x2001/7/20"}       | ${"every"} | ${[1]}      | ${"every"} | ${"every"} | ${["2001/7/20"]}
+${"every monday"} | ${"task x7/20 x2001/7/21"} | ${"every"} | ${[1]}      | ${"every"} | ${"every"} | ${["2001/7/21", "2000/7/20"]}
+`(
+    "toRepetition returns if content is $content",
+    ({ dateString, content, exDay, exDayOfWeek, exWeek, exMonth, exDatesExcepted }) => {
+        const r: Repetition = toRepetition(dateString, content)
+        test(`day is ${exDay}`, () => expect(r.day).toEqual(exDay))
+        test(`dayOfWeek is ${exDayOfWeek}`, () => expect(r.dayOfWeek).toEqual(exDayOfWeek))
+        test(`week is ${exWeek}`, () => expect(r.week).toEqual(exWeek))
+        test(`month is ${exMonth}`, () => expect(r.month).toEqual(exMonth))
+        test(`datesExpected is ${exDatesExcepted}`, () => expect(r.datesExcepted.map(x =>
+                    x.format(FORMAT)
+                )).toEqual(exDatesExcepted))
     }
 )
